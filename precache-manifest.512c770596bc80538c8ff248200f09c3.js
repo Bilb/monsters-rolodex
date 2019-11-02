b239b7395b3dde38ac458efb1abf581055b53afa
Audric Ackermann <audric.ackermann@smile.fr>
2019-11-03 10:42:49 +1100

    Updates

---
+++ precache-manifest.512c770596bc80538c8ff248200f09c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71328007981eb527d32aa72478a8e691",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e4e6980d510fe85570d4",
+    "url": "/monsters-rolodex/static/css/main.3e1afce8.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "e4e6980d510fe85570d4",
+    "url": "/monsters-rolodex/static/js/main.534acb0f.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
