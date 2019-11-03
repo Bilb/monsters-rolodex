5f364bf1ecbc36c07d51bee24b99b59e902fe3d2
Audric Ackermann <audric.ackermann@smile.fr>
2019-11-03 11:04:28 +1100

    respect ratio for monster img

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,6 +5,7 @@ export const Card = props => (
   <div className="card-container">
     <img
       alt="monster"
+      className="monster-img"
       src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180  `}
     />
     <h2>{props.monster.name}</h2>
